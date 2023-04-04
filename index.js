c32e3920bd9b8a1046cbb2e301749513ba628e32
Yer Yang <yyhmg_86@yahoo.com>
2023-04-04 16:48:12 -0700

    Completes Phase 1 Function Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  if (pickupLocation < 42) {
+    return (42 - pickupLocation);
+  } else {
+    return (pickupLocation - 42);
+  }
+}
+function distanceFromHqInFeet(pickupLocation) {
+  return (distanceFromHqInBlocks(pickupLocation) * 264);
+}
+function distanceTravelledInFeet(pointA, pointB) {
+  if (pointA > pointB) {
+    return (pointA - pointB) * 264;
+  } else {
+    return (pointB - pointA) * 264;
+  }
+}
+function calculatesFarePrice(pointA, pointB) {
+  if (distanceTravelledInFeet(pointA, pointB) <= 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(pointA, pointB) > 400
+    && distanceTravelledInFeet(pointA, pointB) < 2000) {
+    return 2.56;
+  } else if (distanceTravelledInFeet(pointA, pointB) >= 2000
+    && distanceTravelledInFeet(pointA, pointB) < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
